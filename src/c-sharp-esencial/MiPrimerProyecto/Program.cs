ad370e5b603e650fe78c254c931e2205e49ffa33
Christian Grimberg <christian.grimberg@outlook.com>
2023-01-12 00:59:21 +0000

    :monkey_face: Tipos de datos y variables por valor y referencia.

--- src/c-sharp-esencial/MiPrimerProyecto/Program.cs
+++ src/c-sharp-esencial/MiPrimerProyecto/Program.cs
@@ -13,11 +13,36 @@ class Program
         Console.WriteLine("Probando mi primer proyecto");
         #endregion
 
+        #region Variables
         string? nombre;
 
         Console.WriteLine("Ingrese su nombre:");
         nombre = Console.ReadLine();
         Console.WriteLine("Tu nombre es: {0}", nombre);
+        #endregion
+
+        #region Tipos de datos
+#pragma warning disable CS0219
+        int positivo = 500;
+        int negativo = -800;
+        float miFloat = 530.60f;
+        double miDoble = 250.50;
+        decimal miDecimal = 835.20m;
+        byte diasSemana = 7;
+        bool miBandera = true;
+        string miCadena = "Probando string";
+        char miChar = miCadena[0];
+#pragma warning restore CS8600
+        #endregion
+
+        #region Variables por valor o referencia
+        bool val = false;
+        bool nuevoVal = val;
+        val = true;
+
+        Console.WriteLine("val {0}", val);
+        Console.WriteLine("nuevoVal {0}", nuevoVal);
+        #endregion
 
         Console.ReadKey();
     }
